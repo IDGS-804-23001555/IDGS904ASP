9eaf1b9ecc29497353daa1bda05d1855a3363de6
Yutzilo <sanchez.yutzil02@gmail.com>
2026-05-21 21:43:21 -0600

    Cinepolis 21/05/26

---
+++ Controllers/CinepolisController.cs
@@ -0,0 +1,21 @@
+﻿using IDGS904ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDGS904ASP.Controllers
+{
+    public class CinepolisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Cinepolis cine)
+        {
+            cine.Calcular();
+
+            return View(cine);
+        }
+    }
+}
---
+++ Models/Cinepolis.cs
@@ -0,0 +1,46 @@
+﻿namespace IDGS904ASP.Models
+{
+    public class Cinepolis
+    {
+        public string Nombre { get; set; }
+        public int Compradores { get; set; }
+        public int Boletos { get; set; }
+        public bool Cineco { get; set; }
+        public double Total { get; set; }
+
+        public void Calcular()
+        {
+            if (Compradores <= 0 || Boletos <= 0)
+            {
+                Total = 0;
+                return;
+            }
+
+            int maxBoletos = Compradores * 7;
+
+            if (Boletos > maxBoletos)
+            {
+                Total = -1;
+                return;
+            }
+
+            double subtotal = Boletos * 12;
+
+            if (Boletos > 5)
+            {
+                subtotal -= subtotal * 0.15;
+            }
+            else if (Boletos >= 3)
+            {
+                subtotal -= subtotal * 0.10;
+            }
+
+            if (Cineco)
+            {
+                subtotal -= subtotal * 0.10;
+            }
+
+            Total = subtotal;
+        }
+    }
+}
